cb265b257ed63f0afc4274e54b3b6a90557b8007
kawshan <kawshan.virantha9999@gmail.com>
2024-08-26 11:43:38 +0530

    angualar practice -v4
    angualar practice -v4 till httpclient provide error

---
+++ src/app/employee.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {EmployeeModel} from "./employee.model";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(public httpClient: HttpClient) { }
+
+
+  api:string = "http://localhost:8080/";
+
+  public saveEmployee(employee:EmployeeModel) : Observable<EmployeeModel>{
+    return this.httpClient.post<EmployeeModel>(`${this.api}/save/employee`, employee);
+  }
+
+}
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,16 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
+import {MatOption, MatSelect} from "@angular/material/select";
+import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatDivider} from "@angular/material/divider";
+import {MatAnchor, MatButton} from "@angular/material/button";
+import {EmployeeService} from "../employee.service";
+import {EmployeeModel} from "../employee.model";
+import {FormsModule, NgForm} from "@angular/forms";
+import {HttpErrorResponse} from "@angular/common/http";
 
 
 @Component({
   selector: 'app-employee',
   standalone: true,
   imports: [
-    MatInputModule
+    MatInputModule,
+    MatSelect,
+    MatOption,
+    MatRadioGroup,
+    MatRadioButton,
+    MatCheckbox,
+    MatDivider,
+    MatButton,
+    MatAnchor,
+    FormsModule
   ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
+  employee:EmployeeModel={
+  employeeAddress: "",
+  employeeContactNUmber: "",
+  employeeDepartment: "",
+  employeeGender: "",
+  employeeId: 0,
+  employeeName: "",
+  employeeSkills: ""
+}
+
+
+
+  constructor(private employeeService: EmployeeService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  saveEmployee(employeeForm:NgForm):void{
+    this.employeeService.saveEmployee(this.employee).subscribe(
+      {
+        next:(res:EmployeeModel)=>{
+          console.log(res);
+          employeeForm.reset();
+          this.employee.employeeGender="";
+        },
+        error:(err:HttpErrorResponse)=>{
+          console.log(err)
+        }
+      }
+    );
+  }
 
 }
